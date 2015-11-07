38553ff50036a54ec73e16966e222bc7a26764c6
cjlovering <cjlovering@130.215.14.76>
2015-11-07 03:23:08 -0500

    added controller package / class [empty]

---
+++ WPI-Nav/src/controller/Controller.java
@@ -0,0 +1,24 @@
+package controller;
+
+/**
+ * Created by Xenocidist on 11/7/15.
+ */
+public class Controller {
+
+    public Controller(){
+        /* on creation, call/organize data ->
+                eventually have a small load screen while this happens
+                do any pre-emptive calculations during
+         */
+    }
+
+    /* function to get all nodes for a map */
+
+    /* function that gets a path, given two+ nodes */
+
+    /* function for handicap path, given two+ nodes */
+
+    /* function looking for nearest three bathrooms */
+
+    /* function that shows three paths to food */
+}
