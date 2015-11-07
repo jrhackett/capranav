b4ff6a9cbccb74f312309629dd8b329ea2b9dc70
Jacob Hackett <jacobrhackett@gmail.com>
2015-11-06 21:08:58 -0500

    refactoring Launcher

---
+++ WPI-Nav/src/Parser.java
@@ -0,0 +1,94 @@
+import javafx.application.Application;
+import javafx.geometry.Insets;
+import javafx.scene.Scene;
+import javafx.scene.control.TableColumn;
+import javafx.scene.control.TableView;
+import javafx.scene.layout.VBox;
+import javafx.stage.Stage;
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.lang.Math;
+import java.util.HashMap;
+import java.util.Scanner;
+import logic.*;
+
+public class Parser
+{
+
+	/*
+	 * Parses nodes and edges from nodes.txt and edges.txt
+	 * into a Graph, returns the graph
+	 */
+	public static Graph parsing()
+	{
+		String inputNodes = "nodes.txt";
+		String inputEdges = "edges.txt";
+		Scanner scanner = null;
+		Scanner scanner2 = null;
+		HashMap<Integer, Node> hashmap = new HashMap<Integer, Node>();
+		int i = 0;
+		
+		try
+		{
+			scanner = new Scanner(new File(inputNodes));
+		}
+		catch(FileNotFoundException e)
+		{
+			e.printStackTrace();
+		}
+		
+		while (scanner.hasNextLine()) 
+		{
+            Scanner s1 = new Scanner(scanner.nextLine());
+            String name = s1.next();
+            int x = Integer.parseInt(s1.next());
+            int y = Integer.parseInt(s1.next());
+            int z = 0; //TODO: change this value later
+            Node node = new Node(name, x, y, z);
+            hashmap.put(i, node);
+            i++;
+		}
+		
+		try
+		{
+			scanner2 = new Scanner(new File(inputEdges));
+		}
+		catch(FileNotFoundException e)
+		{
+			e.printStackTrace();
+		}
+		
+		while(scanner2.hasNextLine())
+		{
+			Scanner s2 = new Scanner(scanner2.nextLine());
+			String firstNodeName = s2.next();
+			String secondNodeName = s2.next();
+			Node node1 = null;
+			Node node2 = null;
+			
+			for(Node node : hashmap.values())
+			{
+				if(node.getName().equals(firstNodeName))
+				{
+					node1 = node;
+				}
+				else if(node.getName().equals(secondNodeName))
+				{
+					node2 = node;
+				}
+			}
+			
+			double weight = Math.sqrt(Math.pow(node1.getX() - node2.getX(),2.0) + Math.pow(node1.getY() - node2.getY(), 2.0));
+			Edge edge = new Edge(node1, weight); //TODO: check this...
+			
+			for(Node node : hashmap.values())
+			{
+				if(node.getName().equals(firstNodeName) || node.getName().equals(secondNodeName))
+				{
+					node.addEdge(edge);
+				}
+			}
+		}
+		return new Graph(hashmap);
+	}
+}
