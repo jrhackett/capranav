4d07586b54a4c2eac86db1413ee7125ced155518
cjlovering <cjlovering@130.215.14.76>
2015-11-09 15:16:31 -0500

    fixed node constructor example data errors

--- WPI-Nav/src/controller/Controller.java
+++ WPI-Nav/src/controller/Controller.java
@@ -39,19 +39,19 @@ public class  Controller {
     }
 
     public static Node[] getMapNodes(String MAP_NAME) {
-        Node n1 = new Node("Institute", 0, 0, 0);
-        Node n2 = new Node("RecCenter", 0, 10, 0);
-        Node n3 = new Node("Field", 0, 20, 0);
-        Node n4 = new Node("Harrington", 3, 16, 0);
-        Node n5 = new Node("Quad", 5, 5, 0);
-        Node n6 = new Node("Morgan", 6, 1, 0);
-        Node n7 = new Node("Riley", 11, 2, 0);
-        Node n8 = new Node("Higgins La bs", 10, 13, 0);
-        Node n9 = new Node("Campus Center", 10, 20, 0);
-        Node n10 = new Node("Fountain", 16, 17, 0);
-        Node n11 = new Node("Alden", 16, 3, 0);
-        Node n12 = new Node("West Street", 18, 8, 0);
-        Node n13 = new Node("Library", 20, 20, 0);
+        Node n1 = new Node("Institute",0, 0, 0, 0);
+        Node n2 = new Node("RecCenter",1, 0, 10, 0);
+        Node n3 = new Node("Field",2, 0, 20, 0);
+        Node n4 = new Node("Harrington",3, 3, 16, 0);
+        Node n5 = new Node("Quad",4, 5, 5, 0);
+        Node n6 = new Node("Morgan",5, 6, 1, 0);
+        Node n7 = new Node("Riley",6, 11, 2, 0);
+        Node n8 = new Node("Higgins La bs",7, 10, 13, 0);
+        Node n9 = new Node("Campus Center",8, 10, 20, 0);
+        Node n10 = new Node("Fountain",9, 16, 17, 0);
+        Node n11 = new Node("Alden",10, 16, 3, 0);
+        Node n12 = new Node("West Street",11, 18, 8, 0);
+        Node n13 = new Node("Library",12, 20, 20, 0);
 
         Node[] nodes = {n1, n2, n3, n4, n5, n6, n7, n8, n9, n10, n11, n12, n13};
         return nodes;
@@ -62,12 +62,12 @@ public class  Controller {
     }
 
     public Node[] getPathNodes(){
-        Node n8 = new Node("Higgins La bs", 10, 13, 0);
-        Node n9 = new Node("Campus Center", 10, 20, 0);
-        Node n10 = new Node("Fountain", 16, 17, 0);
-        Node n11 = new Node("Alden", 16, 3, 0);
-        Node n12 = new Node("West Street", 18, 8, 0);
-        Node n13 = new Node("Library", 20, 20, 0);
+        Node n8 = new Node("Higgins La bs",7, 10, 13, 0);
+        Node n9 = new Node("Campus Center",8, 10, 20, 0);
+        Node n10 = new Node("Fountain",9, 16, 17, 0);
+        Node n11 = new Node("Alden",10, 16, 3, 0);
+        Node n12 = new Node("West Street",11, 18, 8, 0);
+        Node n13 = new Node("Library",12, 20, 20, 0);
         Node[] nodes = {n8, n9, n10, n11, n12, n13};
         return nodes;
     }
