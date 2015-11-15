bb830d72920eaa61e50f756ee888f97afd99277a
jcfriscia <jcfriscia@wpi.edu>
2015-11-15 16:30:53 -0500

    Updates Nodes and Edges, also sets up JUnit testing for AStar

--- WPI-Nav/src/logic/Edge.java
+++ WPI-Nav/src/logic/Edge.java
@@ -19,4 +19,34 @@ public class Edge{
 
     public Node getTarget() { return this.target; }
 
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		result = prime * result + ((target == null) ? 0 : target.hashCode());
+		long temp;
+		temp = Double.doubleToLongBits(weight);
+		result = prime * result + (int) (temp ^ (temp >>> 32));
+		return result;
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Edge other = (Edge) obj;
+		if (target == null) {
+			if (other.target != null)
+				return false;
+		} else if (!target.equals(other.target))
+			return false;
+		if (Double.doubleToLongBits(weight) != Double.doubleToLongBits(other.weight))
+			return false;
+		return true;
+	}
+
 }
