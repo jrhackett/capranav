d4509d5fba518230dc0fcceb1659e6c9f02dcc69
tucker3896 <tucker3896@gmail.com>
2015-11-16 20:50:01 -0500

    added more test cases to step-by-step directions test file. Also implemented english turning directions instead of angle measurements. Also implemented a 'you have arrived' phrase for the final destination.

--- WPI-Nav/src/logic/Directions.java
+++ WPI-Nav/src/logic/Directions.java
@@ -12,7 +12,10 @@ public class Directions {
 	 * @return
 	 */
 	public static ArrayList<String> stepByStep(ArrayList<Node> aStarPath) {
-		ArrayList<String> directions = new ArrayList<String> ();
+		// TODO: Implement skipping of straight path nodes
+		
+		
+		ArrayList<String> directions = new ArrayList<String>();
 
 		// Do special case for first node
 		double dist = Math.sqrt(Math.pow((aStarPath.get(0).getX() - aStarPath.get(1).getX()), 2)
@@ -22,13 +25,39 @@ public class Directions {
 
 		double angle = Math.atan2((aStarPath.get(0).getY() - aStarPath.get(1).getY()),
 				(aStarPath.get(0).getX() - aStarPath.get(1).getX()));
-		
-		angle = Math.round(angle*180/Math.PI - 180);
-		if(angle < 0){
-			angle+= 360;
+
+		angle = Math.round(angle * 180 / Math.PI - 180);
+		if (angle < 0) {
+			angle += 360;
 		}
 
-		directions.add("Face " + Math.round(angle) + ", and walk " + Math.round(dist) + " feet.");
+		// This is used to store the text for a given angle
+		String anglePhrase = "Error";
+
+		// This if block converts the string to cardinal directions
+		// Assumes: East = 0 degrees, South = 90 degrees
+		if (angle >= 0 && angle <= 22)
+			anglePhrase = "East";
+		if (angle > 22 && angle <= 67)
+			anglePhrase = "South-East";
+		if (angle > 67 && angle <= 112)
+			anglePhrase = "South";
+		if (angle > 112 && angle <= 157)
+			anglePhrase = "South-West";
+		if (angle > 157 && angle <= 202)
+			anglePhrase = "West";
+		if (angle > 202 && angle <= 247)
+			anglePhrase = "North-West";
+		if (angle > 247 && angle <= 292)
+			anglePhrase = "North";
+		if (angle > 292 && angle <= 337)
+			anglePhrase = "North-East";
+		if (angle > 337 && angle <= 360)
+			anglePhrase = "East";
+
+		String distPhrase = Math.round(dist) + " feet.";
+
+		directions.add("Face " + anglePhrase + ", and walk " + distPhrase);
 
 		for (int i = 0; i < aStarPath.size() - 2; i++) {
 			Node prev = aStarPath.get(i);
@@ -36,23 +65,32 @@ public class Directions {
 			Node next = aStarPath.get(i + 2);
 
 			// get the distance to the next node and angle
-			dist = Math.sqrt(Math.pow((turn.getX() - prev.getX()), 2) + Math.pow((turn.getY() - prev.getY()), 2));
+			dist = Math.sqrt(Math.pow((turn.getX() - next.getX()), 2) + Math.pow((turn.getY() - next.getY()), 2));
 			totalDistance += dist;
 			angle = getAngle(prev, turn, next);
-			angle = angle*180/Math.PI - 180;
-			if(angle < -180){
-				angle+= 360;
+			angle = angle * 180 / Math.PI - 180;
+			if (angle < -180) {
+				angle += 360;
 			}
-			if(angle > 180){
-				angle-= 360;
+			if (angle > 180) {
+				angle -= 360;
 			}
 
-			directions.add(
-					"Turn " + Math.round(angle) + ", and walk " + Math.round(dist) + " feet.");
+			anglePhrase = AngletoString((int) Math.round(angle));
+			distPhrase = Math.round(dist) + " feet.";
+			// TODO: implement special phrase for when a node is a landmark
+			// if (next is a landmark){
+			// distPhrase = next.specialPhrase;
+			// } else {
+			// distPhrase = Math.round(dist) + " feet.";
+			// }
+
+			directions.add("Turn " + anglePhrase + ", and walk " + distPhrase);
 
 		}
-		// TODO Add english words for angles
-		// TODO Add landmarks in place of distance
+
+		directions.add("You have reached your destination");
+
 		return directions;
 	}
 
@@ -83,20 +121,28 @@ public class Directions {
 	public double getTotalDistance() {
 		return totalDistance;
 	}
-	
-	
-	// This method converts a given angle into the proper string 
-	public static String AngletoString(double angle){
-        if (angle <= 10 && angle >= -10) return "straight ahead"; //straight forward
-        if (angle < -10 && angle >= -35) return"slight left";
-        if (angle < -35 && angle >= -60) return"left";
-        if (angle < -60 && angle >= -110) return"hard left";
-        if (angle < -110 && angle >= -180) return"a near U-turn left";
-        if (angle > 10 && angle <= 35) return"slight right";
-        if (angle > 35 && angle <= 60) return"right";
-        if (angle > 60 && angle <= 110) return"hard right";
-        if (angle > 110 && angle <= 180) return"a near U-turn right";
-        else return"back to an actual coordinate system";
-    }
+
+	// This method converts a given angle into the proper string
+	public static String AngletoString(int angle) {
+		if (angle <= 10 && angle >= -10)
+			return "straight ahead";
+		if (angle < -10 && angle >= -35)
+			return "slight left";
+		if (angle < -35 && angle >= -60)
+			return "left";
+		if (angle < -60 && angle >= -110)
+			return "hard left";
+		if (angle < -110 && angle >= -180)
+			return "a near U-turn left";
+		if (angle > 10 && angle <= 35)
+			return "slight right";
+		if (angle > 35 && angle <= 60)
+			return "right";
+		if (angle > 60 && angle <= 110)
+			return "hard right";
+		if (angle > 110 && angle <= 180)
+			return "a near U-turn right";
+		return String.valueOf(angle);
+	}
 
 }
