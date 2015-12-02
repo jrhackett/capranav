915d9ff5a381fd2b81299ac52652e27beadf8946
Henry Wheeler-Mackta <hjwheelermackta@wpi.edu>
2015-12-01 22:19:59 -0500

    Updated Directions and Email testing

--- WPI-Nav/test/logic/DirectionsTesting.java
+++ WPI-Nav/test/logic/DirectionsTesting.java
@@ -19,14 +19,20 @@ public class DirectionsTesting {
     Landmark nA, nB, nC, nD, nE, nF, nG, nH, nI, nZ;
 
     Campus camp;
+    Floor floo;
     HashMap<Integer, INode> nodes;
-    HashMap<Integer, IMap> m;
+    HashMap<Integer, IMap> mmap, nmap;
 
     @Before
     public void setUp() throws Exception{
-        camp = new Campus(1, "dir/dir/dir", 5.0);
-        m = new HashMap<Integer, IMap>();
-        m.put(camp.getID(), camp);
+        camp = new Campus(1, "dir/dir/dir", 1.0);
+        floo = new Floor(2, "dir/dir/dir", 2.0, 2, 2);
+        mmap = new HashMap<Integer, IMap>();
+        nmap = new HashMap<Integer, IMap>();
+
+        mmap.put(camp.getID(), camp);
+
+        nmap.put(camp.getID(), floo);
 
         // Nodes are set up like this:
         // A - B . C
@@ -37,16 +43,16 @@ public class DirectionsTesting {
         // . . . . |
         // . . . . Z
 
-        nA = new Landmark(1, 0, 0, 0, 0, 0, 0, "A");
-        nB = new Landmark(2, 100, 0, 0, 100, 0, 0, "B");
-        nC = new Landmark(3, 200, 0, 0, 200, 0, 0, "C");
-        nD = new Landmark(4, 0, 100, 0, 0, 100, 0, "D");
-        nE = new Landmark(5, 100, 100, 0, 100, 100, 0, "E");
-        nF = new Landmark(6, 200, 100, 0, 200, 100, 0, "F");
-        nG = new Landmark(7, 0, 200, 0, 0, 200, 0, "G");
-        nH = new Landmark(8, 100, 200, 0, 100, 200, 0, "H");
-        nI = new Landmark(9, 200, 200, 0, 200, 200, 0, "I");
-        nZ = new Landmark(10,200, 300, 0, 200, 300, 0, "Z");
+        nA = new Landmark(1, 0, 0, 0, 0, 0, 0, 1, "A");
+        nB = new Landmark(2, 100, 0, 0, 100, 0, 0, 1, "B");
+        nC = new Landmark(3, 200, 0, 0, 200, 0, 0, 1, "C");
+        nD = new Landmark(4, 0, 100, 0, 0, 100, 0, 1, "D");
+        nE = new Landmark(5, 100, 100, 0, 100, 100, 0, 1, "E");
+        nF = new Landmark(6, 200, 100, 0, 200, 100, 0, 1, "F");
+        nG = new Landmark(7, 0, 200, 0, 0, 200, 0, 1, "G");
+        nH = new Landmark(8, 100, 200, 0, 100, 200, 0, 2, "H");
+        nI = new Landmark(9, 200, 200, 0, 200, 200, 0, 1, "I");
+        nZ = new Landmark(10,200, 300, 0, 200, 300, 0, 1, "Z");
 
         nA.setAdjacencies(new ArrayList<Edge>(Arrays.asList(new Edge(nB.getID(), 1), new Edge(nD.getID(), 1))));
         nB.setAdjacencies(new ArrayList<Edge>(Arrays.asList(new Edge(nA.getID(), 1), new Edge(nE.getID(), 1), new Edge(nF.getID(), 1))));
@@ -77,7 +83,7 @@ public class DirectionsTesting {
     @Test
     public void stepByStepTesting(){
         ArrayList<INode> As = AStarShortestPath.AStarSearch(nA, nB, nodes);
-        ArrayList<ArrayList<Instructions>> aI = Directions.stepByStep(As, m);
+        ArrayList<ArrayList<Instructions>> aI = Directions.stepByStep(As, mmap);
         ArrayList<ArrayList<Instructions>> act = new ArrayList<ArrayList<Instructions>>();
         act.add(new ArrayList<Instructions>());
         act.get(0).add(new Instructions("Face East, and walk 100 feet.", As.get(0)));
@@ -89,7 +95,7 @@ public class DirectionsTesting {
     public void getTotalDistance(){
         ArrayList<INode> As = AStarShortestPath.AStarSearch(nA, nB, nodes);
         Directions d = new Directions();
-        d.stepByStep(As, m);
+        d.stepByStep(As, mmap);
         assertEquals("Total distance is not properly calculated", 100,  d.getTotalDistance(), 0.05);
 
     }
@@ -98,7 +104,7 @@ public class DirectionsTesting {
     public void getTotalDistance2(){
         ArrayList<INode> As = AStarShortestPath.AStarSearch(nA, nE, nodes);
         Directions d = new Directions();
-        d.stepByStep(As, m);
+        d.stepByStep(As, mmap);
         assertEquals("Total distance is not properly calculated", 200,  d.getTotalDistance(), 0.05);
 
     }
@@ -107,7 +113,7 @@ public class DirectionsTesting {
     public void getTotalDistance3(){
         ArrayList<INode> As = AStarShortestPath.AStarSearch(nA, nI, nodes);
         Directions d = new Directions();
-        d.stepByStep(As, m);
+        d.stepByStep(As, mmap);
         assertEquals("Total distance is not properly calculated", 341,  d.getTotalDistance(), 0.5);
 
     }
@@ -116,16 +122,24 @@ public class DirectionsTesting {
     public void getTotalDistance4(){
         ArrayList<INode> As = AStarShortestPath.AStarSearch(nA, nG, nodes);
         Directions d = new Directions();
-        d.stepByStep(As, m);
+        d.stepByStep(As, mmap);
         assertEquals("Total distance is not properly calculated", 341,  d.getTotalDistance(), 0.5);
 
     }
 
     @Test
+    public void getTotalDistance5(){
+        ArrayList<INode> As = AStarShortestPath.AStarSearch(nA, nB, nodes);
+        Directions d = new Directions();
+        d.stepByStep(As, nmap);
+        assertEquals("Total distance is not properly calculated", 200,  d.getTotalDistance(), 0.05);
+
+    }
+    @Test
     public void stepByStepTesting2(){
         ArrayList<INode> As = AStarShortestPath.AStarSearch(nC, nI, nodes);
         Directions d = new Directions();
-        ArrayList<ArrayList<Instructions>> aI = d.stepByStep(As, m);
+        ArrayList<ArrayList<Instructions>> aI = d.stepByStep(As, mmap);
         Instructions i1 = new Instructions("Face South, and walk 100 feet.", As.get(0));
         Instructions i2 = new Instructions("Turn straight ahead, and walk 100 feet.", As.get(1));
         Instructions i3 = new Instructions("You have reached your destination.", As.get(2));
@@ -139,7 +153,7 @@ public class DirectionsTesting {
     public void stepByStepTesting3(){
         ArrayList<INode> As = AStarShortestPath.AStarSearch(nC, nZ, nodes);
         Directions d = new Directions();
-        ArrayList<ArrayList<Instructions>> aI = d.stepByStep(As, m);
+        ArrayList<ArrayList<Instructions>> aI = d.stepByStep(As, mmap);
         Instructions i1 = new Instructions("Face South, and walk 100 feet.", As.get(0));
         Instructions i2 = new Instructions("Turn straight ahead, and walk 200 feet.", As.get(1));
         Instructions i3 = new Instructions("You have reached your destination.", As.get(2));
@@ -153,7 +167,7 @@ public class DirectionsTesting {
     public void stepByStepTesting4(){
         ArrayList<INode> As = AStarShortestPath.AStarSearch(nA, nE, nodes);
         Directions d = new Directions();
-        ArrayList<ArrayList<Instructions>> aI = d.stepByStep(As, m);
+        ArrayList<ArrayList<Instructions>> aI = d.stepByStep(As, mmap);
         Instructions i1 = new Instructions("Face East, and walk 100 feet.", As.get(0));
         Instructions i2 = new Instructions("Turn hard right, and walk 100 feet.", As.get(1));
         Instructions i3 = new Instructions("You have reached your destination.", As.get(2));
@@ -166,7 +180,7 @@ public class DirectionsTesting {
     public void stepByStepTesting5(){
         ArrayList<INode> As = AStarShortestPath.AStarSearch(nD, nG, nodes);
         Directions d = new Directions();
-        ArrayList<ArrayList<Instructions>> aI = d.stepByStep(As, m);
+        ArrayList<ArrayList<Instructions>> aI = d.stepByStep(As, mmap);
 
         Instructions i1 = new Instructions("Face North, and walk 100 feet.", As.get(0));
         Instructions i2 = new Instructions("Turn hard right, and walk 100 feet.", As.get(1));
@@ -182,5 +196,12 @@ public class DirectionsTesting {
         assertEquals("stepByStep did not output correct output", i2c.getInstruction_string(), aI.get(0).get(3).getInstruction_string());
 
         assertEquals("stepByStep did not output correct output", i3.getInstruction_string(), aI.get(0).get(4).getInstruction_string());
+    }
+
+    @Test
+    public void getTotalDistanceTesting2(){
+        ArrayList<INode> As = AStarShortestPath.AStarSearch(nA, nA, nodes);
+        Directions d = new Directions();
+        assertEquals("Total distance not calculated correctly", 0, d.getTotalDistance(), 0.5);
     }
 }
