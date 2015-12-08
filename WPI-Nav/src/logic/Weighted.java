406cb488d96f2f406788da0511a30dca13d3273d
Ksbugbee <ksb30@me.com>
2015-12-07 21:14:38 -0500

    Added weighted class

---
+++ WPI-Nav/src/logic/Weighted.java
@@ -0,0 +1,14 @@
+package logic;
+
+/**
+ * Created by Kurt on 12/7/15.
+ *
+ * This will add weights to the nodes and edges to be able to add delighter features such as
+ * handicapped and weather dependent
+ */
+public class Weighted {
+
+
+
+
+}
