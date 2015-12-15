4df4e45ccb77c02c7cc3ed72a07d40a90e39d0de
jcfriscia <jcfrish@gmail.com>
2015-12-14 19:28:26 -0500

    Adds salisbury polygon

--- WPI-Nav/src/visuals/MapDisplay.java
+++ WPI-Nav/src/visuals/MapDisplay.java
@@ -968,6 +968,20 @@ public class MapDisplay extends Pane {
         library.setFill(Color.TRANSPARENT);
         addPolygonEvents(library, 2148);
 
+        Polygon salisbury = new Polygon();
+        salisbury.getPoints().addAll(new Double[]{
+                441.6495,201.3315,
+                439.5165,214.011,
+                425.889,211.7595,
+                421.0305,241.503,
+                445.323,245.769,
+                443.7825,255.96,
+                463.4535,259.278,
+                472.104,206.427,
+        });
+        salisbury.setFill(Color.TRANSPARENT);
+        addPolygonEvents(salisbury, 1909);//TODO double check this value
+
     }
     private void addPolygonEvents(Polygon p, Integer key){
         this.getChildren().add(p);
